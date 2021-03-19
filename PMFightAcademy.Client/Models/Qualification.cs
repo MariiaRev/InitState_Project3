63715e0adc35b650d2f7d4f6d114641b01d6380a
MariiaRev <maria.revenko2209@gmail.com>
2021-03-19 19:54:06 +0200

    Added Models

---
+++ PMFightAcademy.Client/Models/Qualification.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PMFightAcademy.Client.Models
+{
+    /// <summary>
+    /// Coach qualification. 
+    /// </summary>
+    public class Qualification
+    {
+        /// <summary>
+        /// Qualification id.
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; }
+
+        /// <summary>
+        /// Coach id.
+        /// </summary>
+        [Required]
+        public int CoachId { get; set; }
+
+        /// <summary>
+        /// Service id.
+        /// </summary>
+        [Required]
+        public int ServiceId { get; set; }
+    }
+}
