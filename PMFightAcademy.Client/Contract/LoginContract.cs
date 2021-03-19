3a5aa2e494ee71e7b88b50d91787610debfe07aa
MariiaRev <maria.revenko2209@gmail.com>
2021-03-19 19:55:00 +0200

    Added contracts for registration, login and  paggination

---
+++ PMFightAcademy.Client/Contract/LoginContract.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PMFightAcademy.Client.Contract
+{
+    /// <summary>
+    /// Contract for log in action.
+    /// </summary>
+    public class LoginContract
+    {
+        /// <summary>
+        /// Phone number (login).
+        /// </summary>
+        [Required]
+        [Phone]
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Password.
+        /// </summary>
+        [Required]
+        [MinLength(8)]
+        [RegularExpression("")]
+        public string Password { get; set; }
+    }
+}
