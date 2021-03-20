df5b9973be6e4d4f82ca794cec2fc4c3f6b79908
MariiaRev <maria.revenko2209@gmail.com>
2021-03-20 12:16:10 +0200

    Added mappings for Coach models

--- PMFightAcademy.Client/Mappings/ModelsMapper.cs
+++ PMFightAcademy.Client/Mappings/ModelsMapper.cs
@@ -1,11 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using PMFightAcademy.Client.Contract.Dto;
+using PMFightAcademy.Client.Models;
+using System;
 
 namespace PMFightAcademy.Client.Mappings
 {
-    public class ModelsMapper
+    /// <summary>
+    /// Converts models from <see cref="Models"/> to  their other
+    /// matching models from <see cref="Contract.Dto"/>.
+    /// </summary>
+    public static class ModelsMapper
     {
+        /// <summary>
+        /// Converts <see cref="Coach"/> model to the <see cref="CoachDto"/> one.
+        /// </summary>
+        /// <param name="coach"><paramref name="coach"/> to convert.</param>
+        /// <returns>Converted <see cref="CoachDto"/> coach.</returns>
+        public static CoachDto CoachToCoachDto(Coach coach)
+        {
+            return new CoachDto()
+            {
+                FirstName = coach.FirstName,
+                LastName = coach.LastName,
+                Age = GetAgeByDate(coach.BirthDate),
+                Description = coach.Description,
+                PhoneNumber = coach.PhoneNumber
+            };
+        }
+
+        private static int GetAgeByDate(DateTime date)
+        {
+            return (int)(DateTime.Now.Subtract(date).TotalDays / 365.2425);
+        }
     }
 }
