bdf7d7012a394d3296cff9a1255787ca7ac21380
MariiaRev <maria.revenko2209@gmail.com>
2021-03-21 22:25:21 +0200

    Added one more method to CoachMapping - to map Coach with services to CoachDto

--- PMFightAcademy.Client/Mappings/CoachMapping.cs
+++ PMFightAcademy.Client/Mappings/CoachMapping.cs
@@ -1,6 +1,7 @@
 ﻿using PMFightAcademy.Client.Contract.Dto;
 using PMFightAcademy.Client.Models;
 using System;
+using System.Collections.Generic;
 
 namespace PMFightAcademy.Client.Mappings
 {
@@ -23,6 +24,25 @@ namespace PMFightAcademy.Client.Mappings
                 Age = GetAgeByDate(coach.BirthDate),
                 Description = coach.Description,
                 PhoneNumber = coach.PhoneNumber
+            };
+        }
+
+        /// <summary>
+        /// Converts <see cref="Coach"/> model to the <see cref="CoachDto"/> one.
+        /// </summary>
+        /// <param name="coach"><paramref name="coach"/> to convert.</param>
+        /// <param name="services">Services which can be provided by the coach.</param>
+        /// <returns>Converted <see cref="CoachDto"/> coach.</returns>
+        public static CoachDto CoachWithServicesToCoachDto(Coach coach, IEnumerable<string> services)
+        {
+            return new CoachDto()
+            {
+                FirstName = coach.FirstName,
+                LastName = coach.LastName,
+                Age = GetAgeByDate(coach.BirthDate),
+                Description = coach.Description,
+                PhoneNumber = coach.PhoneNumber,
+                Services = services ?? new List<string>()
             };
         }
 
