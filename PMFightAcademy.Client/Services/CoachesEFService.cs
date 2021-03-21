627a3f5024cf98ee21c4578e8f102822ee844759
MariiaRev <maria.revenko2209@gmail.com>
2021-03-21 15:26:13 +0200

    Added service for getting coaches from DB

---
+++ PMFightAcademy.Client/Services/CoachesEFService.cs
@@ -0,0 +1,31 @@
+﻿using PMFightAcademy.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PMFightAcademy.Client.DataBase;
+
+namespace PMFightAcademy.Client.Services
+{
+    /// <summary>
+    /// Service for getting coaches from database with Entity Framework.
+    /// </summary>
+    public class CoachesEFService : ICoachesStorageService
+    {
+        private readonly ClientContext _dbContext;
+
+        /// <summary>
+        /// Constructor with DI.
+        /// </summary>
+        public CoachesEFService(ClientContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<Coach> GetCoaches(int skipCount, int takeCount)
+        {
+            return _dbContext.Coaches.Skip(skipCount).Take(takeCount);
+        }
+    }
+}
---
+++ PMFightAcademy.Client/Services/ICoachesStorageService.cs
@@ -0,0 +1,20 @@
+﻿using PMFightAcademy.Client.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PMFightAcademy.Client.Services
+{
+    /// <summary>
+    /// Coaches storage service abstraction.
+    /// </summary>
+    public interface ICoachesStorageService
+    {
+        /// <summary>
+        /// Gets coaches list with paggination.
+        /// </summary>
+        /// <param name="skipCount">How many coaches to skip.</param>
+        /// <param name="takeCount">How many coaches to take.</param>
+        /// <returns>Returns list of found coaches or empty list if there is no coach.</returns>
+        IEnumerable<Coach> GetCoaches(int skipCount, int takeCount);
+    }
+}
