d78708f5a43bb830016f6cb14122e0dbe4b13d5a
JesperWerWer <zin4enko.bod@gmail.com>
2021-03-24 12:45:37 +0200

    delete unusing

--- PMFightAcademy.Admin/Services/ServiceInterfaces/IWorkWithIdService.cs
+++ PMFightAcademy.Admin/Services/ServiceInterfaces/IWorkWithIdService.cs
@@ -11,42 +11,6 @@
         /// <param name="id"></param>
         /// <returns></returns>
         public bool IsCorrectId(int id);
-        /// <summary>
-        /// Id for booking
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public int GetIdForBooking();
-        /// <summary>
-        /// Id for coach
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public int GetIdForCoach();
-        /// <summary>
-        /// Id for qualifications
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public int GetIdForQualification();
-        /// <summary>
-        /// id for service
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public int GetIdForService();
-        /// <summary>
-        /// Id for client
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public int GetIdForClient();
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public int GetIdForSlots();
-
+      
     }
 }
--- PMFightAcademy.Admin/Services/WorkWithIdService.cs
+++ PMFightAcademy.Admin/Services/WorkWithIdService.cs
@@ -10,7 +10,7 @@ namespace PMFightAcademy.Admin.Services
     /// <summary>
     /// Work with ID
     /// </summary>
-    public class WorkWithIdService: IWorkWithIdService
+    public class WorkWithIdService : IWorkWithIdService
     {
         private readonly AdminContext _dbContext;
 
@@ -18,7 +18,7 @@ namespace PMFightAcademy.Admin.Services
         /// Work with ID
         /// </summary>
         /// <param name="dbContext"></param>
-        public  WorkWithIdService(AdminContext dbContext)
+        public WorkWithIdService(AdminContext dbContext)
         {
             _dbContext = dbContext;
         }
@@ -28,90 +28,11 @@ namespace PMFightAcademy.Admin.Services
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        public  bool IsCorrectId(int id)
+        public bool IsCorrectId(int id)
         {
             return id >= 0;
         }
 
-        /// <summary>
-        /// RebaseId
-        /// </summary>
-        
-        /// <param name="allId"></param>
-        /// <returns></returns>
-        private static int ReturnNewId(ICollection<int> allId)
-        {
-            if(!allId.Any())
-                return 0;
-            for (var i = 1; i <= allId.Count; i++)
-            {
-                if (!allId.Contains(i))
-                {
-                    return i;
-                }
-            }
 
-            return 0;
-        }
-
-        /// <summary>
-        /// Create new ID
-        /// </summary>
-        /// <returns></returns>
-        public int GetIdForBooking()
-        {
-            var newId = _dbContext.Coaches.Select(x => x.Id).ToList();
-            return ReturnNewId(newId);
-        }
-
-        /// <summary>
-        /// Create new ID
-        /// </summary>
-        /// <returns></returns>
-        public int GetIdForCoach()
-        {
-            var newId = _dbContext.Coaches.Select(x => x.Id).ToList();
-            return ReturnNewId(newId);
-        }
-
-        /// <summary>
-        /// Create new ID
-        /// </summary>
-        /// <returns></returns>
-        public int GetIdForQualification()
-        {
-            var newId = _dbContext.Coaches.Select(x => x.Id).ToList();
-            return ReturnNewId(newId);
-        }
-
-        /// <summary>
-        /// Create new ID
-        /// </summary>
-        /// <returns></returns>
-        public int GetIdForService()
-        {
-            var newId = _dbContext.Coaches.Select(x => x.Id).ToList();
-            return ReturnNewId(newId);
-        }
-
-        /// <summary>
-        /// Create new ID
-        /// </summary>
-        /// <returns></returns>
-        public int GetIdForClient()
-        {
-            var newId = _dbContext.Coaches.Select(x => x.Id).ToList();
-            return ReturnNewId(newId);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public int GetIdForSlots()
-        {
-            var newId = _dbContext.Slots.Select(x => x.Id).ToList();
-            return ReturnNewId(newId);
-        }
     }
 }
