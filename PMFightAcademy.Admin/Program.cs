88d1ecc42523f8a056d96f02ba439f10986a7b6e
vladiolus3 <75817876+vladiolus3@users.noreply.github.com>
2021-03-24 11:31:09 +0200

    move TimeSlotService to a separate class

--- PMFightAcademy.Admin/Program.cs
+++ PMFightAcademy.Admin/Program.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using PMFightAcademy.Admin.DataBase;
 
 namespace PMFightAcademy.Admin
 {
@@ -27,58 +21,5 @@ namespace PMFightAcademy.Admin
                 });
     }
 
-    /// <summary>
-    /// Service for the updating outdated slots in db
-    /// </summary>
-    public class TimerSlotService : IHostedService
-    {
-        private readonly IServiceProvider _serviceProvider;
-
-        public TimerSlotService(IServiceProvider serviceProvider)
-        {
-            _serviceProvider = serviceProvider;
-        }
-
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            Task.Run(() =>
-            {
-                var tm = new TimerCallback(UpdateSlotsTable);
-                var timer = new Timer(tm, null, TimeSpan.FromSeconds(0), TimeSpan.FromHours(1));
-                while (true) { }
-                timer.Dispose();
-            });
-
-            return Task.CompletedTask;
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            return Task.CompletedTask;
-        }
-
-        private async void UpdateSlotsTable(object obj)
-        {
-            using var scope = _serviceProvider.CreateScope();
-            await using var dbContext = scope.ServiceProvider.GetRequiredService<AdminContext>();
-            var slots = await dbContext.Slots.Where(x => x.Expired == false).ToListAsync();
-
-            foreach (var slot in slots)
-            {
-                var slotStart = slot.Date.Add(slot.StartTime);
-                if (slotStart < DateTime.Now)
-                {
-                    slot.Expired = true;
-                    dbContext.Slots.Update(slot);
-                }
-            }
-
-            await dbContext.SaveChangesAsync();
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Table \"SLOTS\" was updated!");
-            Console.ResetColor();
-        }
-    }
 #pragma warning restore 1591
 }
---
+++ PMFightAcademy.Admin/TimerSlotService.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PMFightAcademy.Admin.DataBase;
+
+namespace PMFightAcademy.Admin
+{
+    /// <summary>
+    /// Service for the updating outdated slots in db
+    /// </summary>
+    public class TimerSlotService : IHostedService
+    {
+#pragma warning disable 1591
+        private readonly IServiceProvider _serviceProvider;
+
+        public TimerSlotService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Task.Run(() =>
+            {
+                var tm = new TimerCallback(UpdateSlotsTable);
+                var timer = new Timer(tm, null, TimeSpan.FromSeconds(0), TimeSpan.FromHours(1));
+                while (true) { }
+                timer.Dispose();
+            }, CancellationToken.None);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private async void UpdateSlotsTable(object obj)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            await using var dbContext = scope.ServiceProvider.GetRequiredService<AdminContext>();
+            var slots = await dbContext.Slots.Where(x => x.Expired == false).ToListAsync();
+
+            foreach (var slot in slots)
+            {
+                var slotStart = slot.Date.Add(slot.StartTime);
+                if (slotStart < DateTime.Now)
+                {
+                    slot.Expired = true;
+                    dbContext.Slots.Update(slot);
+                }
+            }
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+#pragma warning restore 1591
+}
