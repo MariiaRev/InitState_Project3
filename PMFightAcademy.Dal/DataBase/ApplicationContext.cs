ceef0e38bab0ac2244ee55d545e5a224fe412637
MariiaRev <maria.revenko2209@gmail.com>
2021-03-26 19:16:22 +0200

    Added migrations at Dal library project

--- PMFightAcademy.Dal/DataBase/ApplicationContext.cs
+++ PMFightAcademy.Dal/DataBase/ApplicationContext.cs
@@ -11,6 +11,7 @@ namespace PMFightAcademy.Dal.DataBase
         public virtual DbSet<Qualification> Qualifications { get; set; }
         public virtual DbSet<Service> Services { get; set; }
         public virtual DbSet<Slot> Slots { get; set; }
+
         public ApplicationContext()
         {
 
@@ -18,6 +19,11 @@ namespace PMFightAcademy.Dal.DataBase
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=db-postgresql-fra1-03704-prod-do-user-8904989-0.b.db.ondigitalocean.com;Port=25060;Database=stagedb;Username=stage;Password=ejsqm3u8zpngy50l;SslMode=Require;trustServerCertificate=true;ApplicationName=PMFA");
         }
     }
 }
