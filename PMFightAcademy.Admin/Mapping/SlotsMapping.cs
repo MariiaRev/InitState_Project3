8fa8a4c867c920015f6d56d80ebde140ddcb49ce
JesperWerWer <zin4enko.bod@gmail.com>
2021-03-26 16:10:16 +0200

    StayPrevius

--- PMFightAcademy.Admin/Mapping/SlotsMapping.cs
+++ PMFightAcademy.Admin/Mapping/SlotsMapping.cs
@@ -62,47 +62,6 @@ namespace PMFightAcademy.Admin.Mapping
 
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="slots"></param>
-        /// <returns></returns>
-        public static IEnumerable<SlotsCreateContract> SlotMapFromModelToContractNewSlotsJS(IEnumerable<Slot> slots)
-        {
-
-            List<SlotsCreateContract> result = new List<SlotsCreateContract>();
-
-            List<List<Slot>> bigSlotsList = new List<List<Slot>>();
-            var dates = slots.Select(x => x.Date).Distinct();
-            var coaches = slots.Select(x => x.CoachId).Distinct();
-
-            foreach (var date in dates)
-            {
-                foreach (var coach in coaches)
-                {
-                    var slotsList = slots.Where(x => x.Date == date).Where(x => x.CoachId == coach).ToList();
-                    bigSlotsList.Add(slotsList);
-                }
-            }
-
-            var count = 0;
-            foreach (var item in bigSlotsList)
-            {
-                var resultItem = item.FirstOrDefault();
-                var lastTime = item.Max(x => x.StartTime);
-                var startTime = item.Min(x => x.StartTime);
-                result.Add(new SlotsCreateContract()
-                {
-                    Id = count,
-                    CoachId = resultItem.CoachId,
-                    DateStart = resultItem.Date.ToString("MM.dd.yyyy"),
-                    TimeEnd = (new DateTime(1, 1, 1) + lastTime).ToString("HH:mm"),
-                    TimeStart = (new DateTime(1, 1, 1) + startTime).ToString("HH:mm")
-                });
-                count++;
-            }
-
-            return result;
-        }
+        
     }
 }
